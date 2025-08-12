2eb2777949171415c6b8de9b998b2ab1ee784051
Shania Jarrisse <shaniajarrissevillanueva@gmail.com>
2025-08-12 10:27:20 +0800

    refactor(core): changed .models into database.models

--- webocr/core/admin.py
+++ webocr/core/admin.py
@@ -5,7 +5,7 @@ from database.models import User  # Import your custom user model
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_active', 'last_login')
+    list_display = ('username', 'email', 'is_active', 'last_login') 
     list_filter = ('is_active', 'last_login')
 
 admin.site.register(User, CustomUserAdmin)  # Register your user model
