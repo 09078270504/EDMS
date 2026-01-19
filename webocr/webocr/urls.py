2fb997924520eb9e1154cb6d8a4821e4adcfc2ea
Shania Jarrisse <shaniajarrissevillanueva@gmail.com>
2026-01-20 05:07:26 +0800

    Redirect /favicon.ico and /favicon.png to a static image to avoid 404s

--- webocr/webocr/urls.py
+++ webocr/webocr/urls.py
@@ -18,9 +18,14 @@ Including another URLconf
 from django.contrib import admin
 #Import for URL patterns
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('api/', include('database.urls')),
+    # Redirect common favicon requests to a static asset to avoid 404s
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/fowse.png'), permanent=True)),
+    path('favicon.png', RedirectView.as_view(url=staticfiles_storage.url('img/fowse.png'), permanent=True)),
 ]
